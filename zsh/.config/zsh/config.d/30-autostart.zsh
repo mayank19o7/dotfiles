1960f5326b395eaeddf01bd94705a4a7c86034af
mayank19o7 <mayank.sahu1907@gmail.com>
2025-09-28 16:00:08 +0530

    fastFetch - load random images, presets for terminal and default output

--- zsh/.config/zsh/config.d/30-autostart.zsh
+++ zsh/.config/zsh/config.d/30-autostart.zsh
@@ -5,6 +5,9 @@
 # -----------------------------------------------------
 # Fastfetch: show system info on pseudo terminals (remote or VM)
 # -----------------------------------------------------
-if [[ $(tty) == *"pts"* ]]; then
-    fastfetch
+if [[ $(tty 2>/dev/null) == *"pts"* ]]; then
+	# ~/.config/fastfetch/presets should have a terminal.jsonrc to be used/auto-started in zsh.
+	if command -v fastfetch >/dev/null 2>&1; then
+		fastfetch --config "$HOME/.config/fastfetch/presets/terminal" 2>/dev/null || true
+	fi
 fi
