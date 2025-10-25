2f02e86d861a37f758397e52fc12d5ba0558b72b
mayank19o7 <mayank.sahu1907@gmail.com>
2025-10-25 21:19:58 +0530

    nvim plugin - treesitter highlighting

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+			indent = { enable = true },
+			highlight = { enable = true },
+			folds = { enable = true },
+			ensure_installed = {
+				"bash",
+				"css",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"python",
+				"toml",
+				"typescript",
+			},
+		})
+	end
+}
