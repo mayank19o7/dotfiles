960875fa714b83b75423245ee681de901f662861
mayank19o7 <mayank.sahu1907@gmail.com>
2025-10-26 22:09:05 +0530

    nvim plugin - noice, pretty cmdline interface and completions

---
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,14 @@
+return {
+	"folke/noice.nvim",
+	opts = {
+		presets = {
+			bottom_search = false,
+			command_palette = false,
+			long_message_to_split = false,
+			inc_rename = false,
+		},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+	},
+}
