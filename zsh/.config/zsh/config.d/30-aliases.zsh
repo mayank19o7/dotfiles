39866fe7824491f754e157e525cdb3484bbec04b
mayank19o7 <mayank.sahu1907@gmail.com>
2025-11-28 22:37:56 +0530

    fzf - modified configuration ( using fd and preview )

--- zsh/.config/zsh/config.d/30-aliases.zsh
+++ zsh/.config/zsh/config.d/30-aliases.zsh
@@ -53,7 +53,7 @@ alias mkdir='mkdir -pv'				# Make directories, show progress, create parents as 
 alias sha256='sha256sum'			# Compute SHA-256 checksum
 alias xtar='tar -xzvf'				# Extract tar archives
 alias ctar='tar -czvf'				# Create tar archives
-alias fzf='fzf --preview "bat -A --color=always --style=numbers --line-range=:500 {}"' # Fuzzy finder Preview
+alias fzf="fzf --preview '$PREVIEW_CMD'" # Fuzzy finder Preview
 
 
 # -------------------------------
