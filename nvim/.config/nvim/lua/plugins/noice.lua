39d238ec7457267760c5490291ff70e346db4a01
mayank19o7 <mayank.sahu1907@gmail.com>
2026-01-03 19:25:34 +0530

    nvim - update noice plugin

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -10,12 +10,14 @@ return {
 				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 			},
 		},
-		-- you can enable a preset for easier configuration
 		presets = {
 			bottom_search = true, -- use a classic bottom cmdline for search
-			command_palette = false, -- position the cmdline and popupmenu together
+			command_palette = false, -- position the cmdline and pop-up menu together
 			long_message_to_split = true, -- long messages will be sent to a split
 			lsp_doc_border = true, -- add a border to hover docs and signature help
 		},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
 	},
 }
